7293f7b235f12891b3d08f38cd62bf4f75a91b83
kywch <choe.kyoung@gmail.com>
2025-02-10 23:26:19 -0800

    roll back pufferl, add norm obs

--- pufferlib/environments/morph/__init__.py
+++ pufferlib/environments/morph/__init__.py
@@ -1,12 +1,23 @@
 from .environment import env_creator
 
+# try:
+#     import torch
+# except ImportError:
+#     pass
+# else:
+#     from .torch import Policy
+#     try:
+#         from .torch import Recurrent
+#     except:
+#         Recurrent = None
+
 try:
-    import torch
+    import pufferlib.environments.morph.policy as torch
 except ImportError:
     pass
 else:
-    from .torch import Policy
+    from .policy import Policy
     try:
-        from .torch import Recurrent
+        from .policy import Recurrent
     except:
         Recurrent = None
