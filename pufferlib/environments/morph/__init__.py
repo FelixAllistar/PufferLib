85cc10e0023a896a59488d5e916c70b35bd1b8bd
kywch <choe.kyoung@gmail.com>
2025-02-11 15:30:15 -0800

    update cfg

--- pufferlib/environments/morph/__init__.py
+++ pufferlib/environments/morph/__init__.py
@@ -1,13 +1,13 @@
 from .environment import env_creator
 
 try:
-    import torch
+    import pufferlib.environments.morph._torch as torch
 except ImportError:
     pass
 else:
-    from .torch import Policy
+    from pufferlib.environments.morph._torch import Policy
     try:
-        from .torch import Recurrent
+        from pufferlib.environments.morph._torch import Recurrent
     except:
         Recurrent = None
 
