aa997d36d8fda393a872e1159223042a82828bee
kywch <choe.kyoung@gmail.com>
2025-02-11 23:04:50 -0800

    try sweep

--- demo.py
+++ demo.py
@@ -186,11 +186,11 @@ def sweep_carbs(args, env_name, make_env, policy_cls, rnn_cls):
         carbs_param('train', 'gae_lambda', 'logit', sweep_parameters, search_center=0.90),
         # carbs_param('train', 'update_epochs', 'linear', sweep_parameters,
         #     search_center=1, scale=3, is_integer=True),
-        carbs_param('train', 'clip_coef', 'logit', sweep_parameters, search_center=0.5),
-        carbs_param('train', 'vf_coef', 'logit', sweep_parameters, search_center=1.0),
-        carbs_param('train', 'vf_clip_coef', 'logit', sweep_parameters, search_center=0.5),
+        # carbs_param('train', 'clip_coef', 'logit', sweep_parameters, search_center=0.5),
+        carbs_param('train', 'vf_coef', 'linear', sweep_parameters, search_center=1.0),
+        # carbs_param('train', 'vf_clip_coef', 'logit', sweep_parameters, search_center=0.5),
         carbs_param('train', 'max_grad_norm', 'linear', sweep_parameters, search_center=1.0),
-        carbs_param('train', 'ent_coef', 'log', sweep_parameters, search_center=0.0001),
+        # carbs_param('train', 'ent_coef', 'log', sweep_parameters, search_center=0.0001),
         # carbs_param('train', 'batch_size', 'log', sweep_parameters,
         #     search_center=default_batch, is_integer=True),
         # carbs_param('train', 'minibatch_size', 'log', sweep_parameters,
@@ -236,10 +236,10 @@ def sweep_carbs(args, env_name, make_env, policy_cls, rnn_cls):
         train_suggestion = {k.split('/')[1]: v for k, v in suggestion.items() if k.startswith('train/')}
         env_suggestion = {k.split('/')[1]: v for k, v in suggestion.items() if k.startswith('env/')}
         args['train'].update(train_suggestion)
-        args['train']['batch_size'] = closest_power(
-            train_suggestion['batch_size'])
-        args['train']['minibatch_size'] = closest_power(
-            train_suggestion['minibatch_size'])
+        # args['train']['batch_size'] = closest_power(
+        #     train_suggestion['batch_size'])
+        # args['train']['minibatch_size'] = closest_power(
+        #     train_suggestion['minibatch_size'])
         args['train']['bptt_horizon'] = closest_power(
             train_suggestion['bptt_horizon'])
 
